d73843acc3a9ecdf5667f5fae93faa10192db975
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-01-21 11:24:42 +0100

    refactor script

--- lib/main.ts
+++ lib/main.ts
@@ -1,23 +1,73 @@
 import * as _ from 'lodash';
-import { ethers } from 'ethers';
-import { createInterface } from 'readline';
+import { Provider                            } from '@ethersproject/abstract-provider';
+import { Signer                              } from '@ethersproject/abstract-signer';
+import { Contract                            } from '@ethersproject/contracts';
+import { LedgerSigner                        } from '@ethersproject/hardware-wallets';
+import { isValidMnemonic, defaultPath        } from '@ethersproject/hdnode';
+import { getDefaultProvider                  } from '@ethersproject/providers';
+import { Wallet                              } from '@ethersproject/wallet';
+import { createInterface                     } from 'readline';
+import yargs                                   from 'yargs';
 
 const { abi } = require('../build/contracts/KYC.json');
 
-
-// ------------[ Configuration - Begin ]------------
-const network:    string = process.env.CHAIN || "goerli";
-const address:    string = process.env.PROXY || "erlc.iexec.eth";
-const privatekey: string = process.env.MNEMONIC;
-// ------------[  Configuration - End  ]------------
-
-
-
 (async () => {
-	let provider: ethers.providers.Provider = ethers.getDefaultProvider(network);
-	let wallet:   ethers.Wallet             = new ethers.Wallet(privatekey, provider);
-	let contract: ethers.Contract           = new ethers.Contract(address, abi, wallet);
-	let entries:  string[]                  = new Array();
+	const argv = yargs
+		.option('network', {
+			type:        'string',
+			describe:    'select blockchain to connect to by name or endpoint',
+			default:     process.env.CHAIN ?? 'mainnet',
+		})
+		.option('address', {
+			type:        'string',
+			describe:    'select whitelist contract to operate on contract (hex or ENS address)',
+			default:     process.env.PROXY ?? 'erlc.iexec.eth',
+		})
+		.option('ledger', {
+			type:        'boolean',
+			describe:    'Use a ledger hardware provider to sign transaction',
+			conflicts:   [ 'mnemonic', 'privatekey' ],
+		})
+		.option('mnemonic', {
+			type:        'string',
+			describe:    'Use a mnemonic to sign transaction',
+			requiresArg: true,
+			coerce:    mn => {
+				if (!isValidMnemonic(mn)) throw new Error('invalid mnemonic format');
+				return mn;
+			},
+			conflicts:   [ 'ledger', 'privatekey' ],
+		})
+		.option('privatekey', {
+			type:        'string',
+			describe:    'Use a privatekey to sign transaction',
+			requiresArg: true,
+			conflicts:   [ 'ledger', 'mnemonic' ],
+		})
+		.option('path', {
+			type:        'string',
+			describe:    'HD path to use when connecting with a ledger or a mnemonic',
+			default:     defaultPath,
+		})
+		.help()
+		.argv;
+
+	let provider : Provider = getDefaultProvider(argv.network);
+	let wallet   : Signer;
+
+	if (argv.ledger) {
+		wallet = new LedgerSigner(provider, 'hid', argv.path);
+	} else if (argv.mnemonic) {
+		wallet = Wallet.fromMnemonic(argv.mnemonic, argv.path).connect(provider);
+	} else if (argv.privatekey) {
+		wallet = new Wallet(argv.privatekey, provider);
+	} else {
+		console.error('Error: no connection mechanism specified');
+		process.exit(1);
+	}
+
+	let contract = new Contract(argv.address, abi, wallet);
+	let entries  = new Array();
 
 	createInterface({
 		input:    process.stdin,
@@ -29,10 +79,11 @@ const privatekey: string = process.env.MNEMONIC;
 	})
 	.on('close', () => {
 		_.chunk(entries, 32).reduce(
-			(promise, chunk) => new Promise((resolve, reject) => {
+			(promise, chunk, i, { length }) => new Promise((resolve, reject) => {
 				Promise.resolve(promise)
 				.then(() => {
-					console.log({ chunk })
+					console.log(`Processing chunk ${i+1}/${length} ...`);
+					console.log(chunk);
 					contract.grantKYC(chunk)
 					.then(({ wait }) => wait().then(resolve).catch(reject))
 					.catch(reject)
