6c4257cb9826ae5921b24ae9be880a6dac63464c
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-01-21 12:03:59 +0100

    minor cleanup

--- lib/main.ts
+++ lib/main.ts
@@ -1,55 +1,55 @@
-import * as _ from 'lodash';
-import { Provider                            } from '@ethersproject/abstract-provider';
-import { Signer                              } from '@ethersproject/abstract-signer';
-import { Contract                            } from '@ethersproject/contracts';
-import { LedgerSigner                        } from '@ethersproject/hardware-wallets';
-import { isValidMnemonic, defaultPath        } from '@ethersproject/hdnode';
-import { getDefaultProvider                  } from '@ethersproject/providers';
-import { Wallet                              } from '@ethersproject/wallet';
-import { createInterface                     } from 'readline';
-import yargs                                   from 'yargs';
+import { chunk                        } from 'lodash';
+import { Provider                     } from '@ethersproject/abstract-provider';
+import { Signer                       } from '@ethersproject/abstract-signer';
+import { Contract                     } from '@ethersproject/contracts';
+import { LedgerSigner                 } from '@ethersproject/hardware-wallets';
+import { isValidMnemonic, defaultPath } from '@ethersproject/hdnode';
+import { getDefaultProvider           } from '@ethersproject/providers';
+import { Wallet                       } from '@ethersproject/wallet';
+import { createInterface              } from 'readline';
+import yargs                            from 'yargs';
 
 const { abi } = require('../build/contracts/KYC.json');
 
 (async () => {
 	const argv = yargs
+		.help()
+		.group([ 'network', 'address' ], 'Blockchain:')
 		.option('network', {
 			type:        'string',
-			describe:    'select blockchain to connect to by name or endpoint',
-			default:     process.env.CHAIN ?? 'mainnet',
+			describe:    'select blockchain to connect to (name or endpoint)',
+			default:     'mainnet',
 		})
 		.option('address', {
 			type:        'string',
 			describe:    'select whitelist contract to operate on contract (hex or ENS address)',
-			default:     process.env.PROXY ?? 'erlc.iexec.eth',
+			default:     'erlc.iexec.eth',
 		})
+		.group([ 'privatekey', 'mnemonic', 'ledger' ], 'Wallets:')
 		.option('ledger', {
 			type:        'boolean',
-			describe:    'Use a ledger hardware provider to sign transaction',
+			describe:    'sign transaction with a ledger hardware provider',
 			conflicts:   [ 'mnemonic', 'privatekey' ],
 		})
 		.option('mnemonic', {
 			type:        'string',
-			describe:    'Use a mnemonic to sign transaction',
+			describe:    'sign transaction with a mnemonic',
 			requiresArg: true,
-			coerce:    mn => {
-				if (!isValidMnemonic(mn)) throw new Error('invalid mnemonic format');
-				return mn;
-			},
+			coerce:      mn => { if (!isValidMnemonic(mn)) throw new Error('invalid mnemonic format'); return mn; },
 			conflicts:   [ 'ledger', 'privatekey' ],
 		})
 		.option('privatekey', {
 			type:        'string',
-			describe:    'Use a privatekey to sign transaction',
+			describe:    'sign transaction with a privatekey',
 			requiresArg: true,
 			conflicts:   [ 'ledger', 'mnemonic' ],
 		})
+		.group([ 'path' ], 'Wallets settings:')
 		.option('path', {
 			type:        'string',
 			describe:    'HD path to use when connecting with a ledger or a mnemonic',
 			default:     defaultPath,
 		})
-		.help()
 		.argv;
 
 	let provider : Provider = getDefaultProvider(argv.network);
@@ -66,8 +66,8 @@ const { abi } = require('../build/contracts/KYC.json');
 		process.exit(1);
 	}
 
-	let contract = new Contract(argv.address, abi, wallet);
-	let entries  = new Array();
+	let contract : Contract = new Contract(argv.address, abi, wallet);
+	let entries  : string[] = new Array();
 
 	createInterface({
 		input:    process.stdin,
@@ -78,7 +78,7 @@ const { abi } = require('../build/contracts/KYC.json');
 		entries.push(entry);
 	})
 	.on('close', () => {
-		_.chunk(entries, 32).reduce(
+		chunk(entries, 32).reduce(
 			(promise, chunk, i, { length }) => new Promise((resolve, reject) => {
 				Promise.resolve(promise)
 				.then(() => {
@@ -86,11 +86,11 @@ const { abi } = require('../build/contracts/KYC.json');
 					console.log(chunk);
 					contract.grantKYC(chunk)
 					.then(({ wait }) => wait().then(resolve).catch(reject))
-					.catch(reject)
+					.catch(reject);
 				})
 				.catch(reject);
 			}),
-			Promise.resolve(null)
+			Promise.resolve()
 		).then(() => console.log(`Processed ${entries.length} addresses`));
 	});
 
